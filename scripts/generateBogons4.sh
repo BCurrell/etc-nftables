372bb367bd9e95c3fe5ede735f7ea085e3cb8550
Brandon Currell <45363644+BCurrell@users.noreply.github.com>
2021-02-27 17:44:26 +0000

    Start work on bogons automation

---
+++ scripts/generateBogons4.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+URL="https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt"
+OUTPUT="/etc/nftables/vars/bogons_v4.nft"
---
+++ scripts/generateBogons6.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+URL="https://www.team-cymru.org/Services/Bogons/fullbogons-ipv6.txt"
+OUTPUT="/etc/nftables/vars/bogons_v6.nft"
